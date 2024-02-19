2ad6e495167fee470e4bece0d3a12742bf0ad41e
ufik0203@gmail.com <ufik0203@gmail.com>
2024-02-19 18:07:22 +0700

    commit

---
+++ OOP-Polymophism/src/oop/polymophism/bunga.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop.polymophism;
+
+/**
+ *
+ * @author mario
+ */
+public class bunga {
+    public void ciri() {
+        System.out.println("Nama dan ciri - ciri bunga : ");
+    }
+}
---
+++ OOP-Polymophism/src/oop/polymophism/mawar.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop.polymophism;
+
+/**
+ *
+ * @author mario
+ */
+    
+public class mawar extends bunga {
+
+    public void ciri() {
+        System.out.println("==== MAWAR ====");
+        System.out.println("- Memiliki duri pada tangkainya");
+        System.out.println("- Memiliki kelopak seperti mahkota");
+        System.out.println("- Memiliki bau yang wangi");
+        System.out.println("- jenis populer yaitu mawar putih dan mawar merah");
+    }
+}
